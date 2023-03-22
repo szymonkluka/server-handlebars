a312d1f1507468faeb1d80355aa5790189d75c16
Szymon Kluka <szymonkluka@onet.pl>
2023-03-22 17:12:08 +0100

    express-app-moustaches

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+
+const app = express();
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/hello/:name', (req, res) => {
+    res.render('hello');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', { layout: 'black' });
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+    res.render('info');
+});
+
+app.get('/history', (req, res) => {
+    res.render('history');
+});
+
+app.use((req, res) => {
+    res.status(404).send('404 not found...');
+})
+
+app.listen(8000, () => {
+    console.log('Server is running on port: 8000');
+});
