8eabd7be7bdd531dc0c9cd2e3e303bd3c6c45ff7
Szymon Kluka <szymonkluka@onet.pl>
2023-03-23 16:27:20 +0100

    http-server-application

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+const upload = multer();
 
 const app = express();
 app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/contact/send-message', upload.single('picture'), (req, res) => {
+
+    const { author, sender, title, message } = req.body;
+
+    if (author && sender && title && message) {
+        res.render('contact', { isSent: true });
+    }
+    else {
+        res.render('contact', { isError: true });
+    }
+
+});
 
 app.get('/', (req, res) => {
     res.render('index');
